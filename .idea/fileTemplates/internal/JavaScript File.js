eddb92357214d9a623bc20b7ff49be9e864cf331
ThomasR <thomasr@fantasymail.de>
2016-02-07 20:49:03 +0100

    Add file templates for IDEA

---
+++ .idea/fileTemplates/internal/JavaScript File.js
@@ -0,0 +1,14 @@
+/**!
+#parse("SEITENBAU Apache License.txt")
+
+*/
+
+'use strict';
+
+// const  = require('');
+// const  = require('');
+
+// const  = require('./');
+// const  = require('./');
+
+// module.exports = #[[$END$]]#;
